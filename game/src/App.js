aceccd16feed040a906ae720675faf0d6113efb4
PriySangwan <120953244+PriySangwan@users.noreply.github.com>
2024-05-03 17:17:09 +0530

    Initial Commit

---
+++ game/src/App.js
@@ -0,0 +1,18 @@
+//App.js
+
+import React from 'react';
+
+import GameComponent from './GameComponent';
+
+function App() {
+
+    return (
+
+        <div className="App">
+
+            <GameComponent />
+
+        </div>);
+}
+
+export default App;
